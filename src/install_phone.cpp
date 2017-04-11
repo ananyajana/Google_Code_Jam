59f2f980babade5c2a45899f699dab844bf388a1
ananyajana <ananya.jana@gmail.com>
2017-04-11 17:14:37 +0530

    incomplete

---
+++ src/install_phone.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int T=0, B=0, D=0, i, j;
+    unsigned long long* D_arr = NULL;
+    
+
+	FILE* fp = fopen("ip.txt", "r");
+	
+	fscanf(fp, "%d", &T);
+	printf("T = %d\n", T);	
+	
+	for(int t = 1; t <= T; ++t){
+        fscanf(fp, "%d", &B);   
+	    printf("B = %d\n", B);	
+        D_arr = (unsigned  long long)calloc(B, sizeof(unsigned long long));
+        for(i = 0; i < B; ++i)
+            for(j = 0; j < i; ++j)
+                fscanf(fp, "%llu", &D_arr[i][j]);
+
+		printf("Case #%d: %s\n", t, D);
+	}
+}
+				
+		
+		
