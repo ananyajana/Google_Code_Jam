b1d50c7d0db31d46204caba4f59e71e3b99899ce
Ananya Jana <ananya.jana@gmail.com>
2017-05-05 14:10:38 +0530

    incomplete verson

--- src/codys_jam_2017.cpp
+++ src/codys_jam_2017.cpp
@@ -5,9 +5,18 @@ Author:Ananya Jana
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node{
+	int price;
+	struct node* next;
+} Node;
+
 int main()
 {
     int T = 0, N = 0;
+    Node** start;
+	Node* counter, search_ptr;
+    start = counter = search_ptr = NULL;
+	int *num_array = NULL;
 
     FILE* fp = fopen("ip.txt", "r");
 
@@ -17,7 +26,12 @@ int main()
     for(int t = 1; t <= T; ++t){
 
         fscanf(fp, "%d", &N);
+        num_array = (int *)calloc(2 * N, sizeof(int));
+        for(int i = 0; i < 2 * N; ++i){
+        	fscanf(fp, "%d", &num_arry[i]);
+		}
         
+		        
         printf("case #%d:\n", t);
     }
 }
