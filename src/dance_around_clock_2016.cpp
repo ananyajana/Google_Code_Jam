4084327b35c3a291cf88e6b3027b64ab45c3733a
Ananya Jana <ananya.jana@gmail.com>
2017-05-15 15:09:14 +0530

    incomplete version

--- src/dance_around_clock_2016.cpp
+++ src/dance_around_clock_2016.cpp
@@ -17,18 +17,39 @@ Author:Ananya Jana
 
 int main()
 {
-    int T, N, K, D;
-    T = N = D = K = 0;
+    int T, N, K, D, temp, partner_left, partner_right;
+    T = N = D = K = temp = partner_left = partner_right = 0;
 
     FILE* fp = fopen("ip.txt", "r");
 
     fscanf(fp, "%d", &T);
 
     for(int t = 1; t <= T; ++t){
-
+		// input the number of dancers, the number of one of the dancers and the number of turns the dance will go on
         fscanf(fp, "%d %d %d", &D, &K, &N);
         
+        // number of pairs that can be formed from the D dancers
+        temp = D / 2;
+        
+        // since the pattern of the dancers repeat after every D/2 number of turns, so we take only the remainder 
+		if( N > temp){
+        	N = N % temp;
+		}
 		
+		//calculate the initial partners of the dancer K
+		if(K == D){
+			partner_left = K - 1;
+			partner_right = 1;
+		}
+		else if(K == 1){
+			partner_left = D;
+			partner_right = K + 1;
+		}
+		else{
+			partner_left = K - 1;
+			partner_right = K + 1;
+		}
+			
 		printf("Case #%d:", t);
     }
 }
