2d13bbc2f85cdeab3eb42c5d7bacde9b937a10f0
Ananya Jana <ananya.jana@gmail.com>
2017-05-15 14:06:48 +0530

    checking in the logic of the solution

--- src/dance_around_clock_2016.cpp
+++ src/dance_around_clock_2016.cpp
@@ -1,4 +1,13 @@
 /*  Google Code Jam I/O women 2016, problem B: Dance Around the Clock
+Observation 1: If there are D number of dancers, then the initial pattern would repeat
+after D/2 changes.
+Observation 2: For even numbered dancers, the partners would always be odd numbered and
+vice versa.
+Observation 3: For odd numbered dancers, the partners would be one of - (10, 2), (2, 4), (4, 6), (6, 8), (8, 10)
+Similaryly for odd numbered "	,		"		"		"	"	"	" - (1, 3), (9, 1), (7, 9), (5, 7), (3, 5) 
+(Assuming there are only 10 dancers)
+Observation 4: Initially the partners of dancer n would be (n-1) and (n+1) and from then incrementing one at each
+step until wrapping up once they cross the range <1 - D>
 Author:Ananya Jana
 */
 
