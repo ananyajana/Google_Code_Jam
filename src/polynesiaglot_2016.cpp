ea592b9e4f5785de442d1ce3682c1757ff7bf4a9
Ananya Jana <ananya.jana@gmail.com>
2017-05-22 10:22:11 +0530

    checking in  the initial logic

--- src/polynesiaglot_2016.cpp
+++ src/polynesiaglot_2016.cpp
@@ -1,10 +1,13 @@
 /*  Google Code Jam I/O women 2016, problem C: Polynesiaglot
 First find out the total number of words: (V+C) to the power L(since in any of the position we can have any of V+C letters)
 To calculate the total number of invalid words, we make  an observation that for any word to be invald, there is at least
-one VV inside it or it ends with V. 
-The number of words having at least one VV is V square * (V + C) to the power L-2(since other positions can be filled with 
-any other letter) * (L - 1), since this VV can appear in any one of the places from 1 to L - 1.
-The number of words ending with V is V*(V+C) to the power (L - 1)
+one CC inside it or it ends with C. 
+The number of words having at least one CC is C square * (V + C) to the power L-2(since other positions can be filled with 
+any other letter) * (L - 1), since this CC can appear in any one of the places from 1 to L - 1.
+The number of words ending with C is C*(V+C) to the power (L - 1)
+
+Since this is a recusrsive formula, memoization is used.
+We will take the modulo value at each step.
 Author:Ananya Jana
 */
 
