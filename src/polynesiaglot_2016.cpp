d6f49c782aa43f8d14dc4946e2eb7045fec31f9c
Ananya Jana <ananya.jana@gmail.com>
2017-05-23 17:03:59 +0530

    checking in the alternative logic
    Signed-off-by: Ananya Jana <ananya.jana@gmail.com>

--- src/polynesiaglot_2016.cpp
+++ src/polynesiaglot_2016.cpp
@@ -6,6 +6,22 @@ The number of words having at least one CC is C square * (V + C) to the power L-
 any other letter) * (L - 1), since this CC can appear in any one of the places from 1 to L - 1.
 The number of words ending with C is C*(V+C) to the power (L - 1)
 
+In this approach we need to determine the intersection between the two sets: The number of words ending in a consonant and the number of
+words having at least two consecutive consonants.
+
+Other appraach(from google code jam contest analysis):
+W(X) = Wc(X) + Wv(X)
+W(X) - total number of valid words of length X
+Wc(X) - total number of words of length X and beginning with a consonant
+Wv(X) - "						"								vowel
+
+Wc(X) = C * Wv(X - 1)
+Wv(X) = V * W(X - 1)
+
+Hence
+W(X) = V * W(X - 1) + V * C * W(X - 2)
+ 
+
 Since this is a recusrsive formula, memoization is used.
 We will take the modulo value at each step.
 Author:Ananya Jana
