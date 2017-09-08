985bac77124bbf0c0f7d5c791f82fc6fba2572be
Ananya Jana <ananya.jana@gmail.com>
2017-09-08 08:06:39 +0530

    initial check-in

---
+++ src/bathroom_stalls_2017.cpp
@@ -0,0 +1,28 @@
+/*  Google Code Jam 2017, problem C: Bathroom Stalls
+Author:Ananya Jana
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX	4
+
+int main()
+{
+	
+	FILE* fp = fopen("ip.txt", "r");
+	
+	// if the file is not valid, exit
+	if(NULL == fp){
+		printf("Error, Can't open file");
+		exit(1);
+	}
+	
+    fscanf(fp, "%d", &T);
+	
+    for(int t = 1; t <= T; ++t){
+    	fscanf(fp, "%d", &input);
+    	
+    	
+	}
+}
